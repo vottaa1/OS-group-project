41453b0fb187ac32fd3116de1a69fd3d7a6a071f
annanguyen2671603 <anhnguyen2671603@gmail.com>
2025-10-31 04:26:21 -0400

    Add files via upload

--- RaceTrack.java
+++ RaceTrack.java
@@ -1,136 +1,158 @@
+package Game;
 import javax.swing.*;
+
 import java.awt.*;
 import java.util.List;
 import java.util.ArrayList;
 
 public class RaceTrack extends JPanel {
 
-	//initalize variables to draw grid
     private final int rows = 10;
     private final int cols = 30;
-    private final int startCol = 1;
-    private final int finishCol = cols-1;
-    private final int[][] grid = new int[rows][cols]; // 0 = empty, 1 = car1, 2 = car2, 3 = finish
+    private final int START_COL = 1;
+    private final int FINISH_COL = cols - 1;
+    private final int[][] grid = new int[rows][cols]; // just for visuals
+    
+    //Synchronization for trackLock and winnerDeclaration
+    private final Synchronization.CustomLock trackLock = new Synchronization.CustomLock();
+    private final Synchronization.WinnerFlag winnerFlag = new Synchronization.WinnerFlag();
+    
 
-    //initalize list of cars 
-    private final List<car> cars =new ArrayList<>();
+    private final List<Car> cars = new ArrayList<>();
     private final Color[] colors = {Color.RED, Color.BLUE, Color.ORANGE, Color.CYAN};
 
     public RaceTrack(int numCars) {
-        // Set finish line col
+        // Mark start and finish lines
         for (int r = 0; r < rows; r++) {
-            grid[r][finishCol] = 4; // finish line
+            grid[r][START_COL] = 3;
+            grid[r][FINISH_COL] = 4;
         }
-        
-        // set starting line 
-        for (int r=0; r<rows; r++) {
-        	grid[r][1]=3;
-        }
-        
-        //add cars to track 
-        int avalibleRows = rows;
-        for (int i = 0; i<numCars; i++) {
-        	//evenly spaces cars out based on how many cars there are. 
-        	int row = i* (avalibleRows -1)/ (numCars-1); 
-        	Color color= colors[i%colors.length]; // cars colors alternate
-        	cars.add(new car (row, color)); //add new car to track
-        }
-       
-    }
-    //use java synchronize methods to prevent race conditions
-    public synchronized boolean moveCar(car c) { //car moves forward if it is not at the finish line
-        int nextCol = c.col + 1;
 
-        if (nextCol >= finishCol) { //if cars current location is at the finish line
-            c.col = finishCol;
+        if (numCars <= 0) numCars = 1;
+        for (int i = 0; i < numCars; i++) {
+            int row = (numCars == 1) ? rows / 2 : (i * (rows - 1)) / (numCars - 1);
+            Color color = colors[i % colors.length];
+            cars.add(new Car(row, color));
+        }
+    }
+
+    public boolean moveCar(Car c) {
+    	trackLock.lock();
+    	try {
+            if (winnerFlag.getWinner() != null) return false;
+
+            int nextCol = c.col + 1;
+
+            if (nextCol >= FINISH_COL) {
+                c.col = FINISH_COL;
+                SwingUtilities.invokeLater(this::repaint);
+                declareWinner(c);
+                return false; // finished
+            }
+
+            // Simple collision prevention (prevents two cars from occupying the same space)
+            for (Car other : cars) {
+                if (other != c && other.row == c.row && other.col == nextCol) {
+                    // Car cannot move forward, stays in place this turn
+                    return true; 
+                }
+            }
+
+            c.col = nextCol;
             SwingUtilities.invokeLater(this::repaint);
-            return false; // finished
+            return true;
+        } finally {
+            trackLock.unlock();
         }
-
-        // prevent overlap
-        for (car other : cars) {
-            if (other != c && other.row == c.row && other.col == nextCol) return true;
-        }
-
-        // car is not at finish line, move forward one col
-        c.col = nextCol;
-        SwingUtilities.invokeLater(this::repaint);
-        return true;
+    }
+    
+    public boolean isRaceOver() {
+    	return winnerFlag.getWinner() != null;
+    }
+    
+    //Synchronization for the finish line
+    //Make sure that no car can enter if there is a winner
+    public void declareWinner(Car c) {
+        winnerFlag.setWinner(c);
     }
 
-    //reset race, all cars go back to start 
-    public synchronized void resetCarsToStart() {
-        for (car c : cars) c.col = 0;
-        repaint();
+    public void resetCarsToStart() {
+        trackLock.lock();
+        try {
+            for (Car c : cars) c.col = START_COL;
+            winnerFlag.reset();
+            repaint();
+        } finally {
+            trackLock.unlock();
+        }
     }
-       
- //draw graphics 
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        
+     // Acquire lock to read the current state of car positions
+        trackLock.lock();
+        try {
 
         int cellWidth = getWidth() / cols;
         int cellHeight = getHeight() / rows;
-        int startCol =1; //col for starting line 
-        int finishCol= cols-1; //col for finish line
 
-//drawing track
-        //nested for loop to make grid 
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                
-                // draw starting/finish lines 
-                if (c == startCol) g.setColor(Color.GREEN); //start line
-                else if (c == finishCol) g.setColor(Color.BLACK);      // finish line
-                else  g.setColor(Color.WHITE); // empty space on track
-                g.fillRect(c * cellWidth, r * cellHeight, cellWidth, cellHeight);
-                
-                g.fillRect(c * cellWidth, r * cellHeight, cellWidth, cellHeight);
+                if (c == START_COL) g.setColor(Color.GREEN);
+                else if (c == FINISH_COL) g.setColor(Color.BLACK);
+                else g.setColor(Color.WHITE);
 
-                g.setColor(Color.GRAY); // grid lines
+                g.fillRect(c * cellWidth, r * cellHeight, cellWidth, cellHeight);
+                g.setColor(Color.GRAY);
                 g.drawRect(c * cellWidth, r * cellHeight, cellWidth, cellHeight);
-                
-                }
-       
             }
-            //draw cars 
-            for (car car:cars) {
-            	g.setColor(car.color);
-            	g.fillRect(car.col * cellWidth, car.row * cellHeight, cellWidth, cellHeight);
         }
 
-     // Draw START text vertically
-        Graphics2D g2 = (Graphics2D) g.create(); // create a copy to avoid rotating everything
+        for (Car c : cars) {
+            g.setColor(c.color);
+            g.fillRect(c.col * cellWidth, c.row * cellHeight, cellWidth, cellHeight);
+        }
+        
+        // Draw Winner Text
+        Car winner = winnerFlag.getWinner();
+        if (winner != null) {
+            g.setColor(winner.color); // Color would change depends on which car is the winner
+            g.setFont(new Font("Inter", Font.BOLD, 30));
+            String winText = "Winner: Car on Row " + (winner.row+1) + "!";
+            FontMetrics fm = g.getFontMetrics();
+            int textX = (getWidth() - fm.stringWidth(winText)) / 2;
+            g.drawString(winText, textX, getHeight() - 10);
+        }
+
+        // START label
+        Graphics2D g2 = (Graphics2D) g.create();
         g2.setColor(Color.WHITE);
         g2.setFont(new Font("Arial", Font.BOLD, 16));
-        // Rotate text 90° counter-clockwise 
-        g2.rotate(-Math.PI / 2, startCol * cellWidth +16, getHeight() / 2);
-        g2.drawString("START", startCol * cellWidth, getHeight() / 2);
-        g2.dispose(); // restore original graphics
+        g2.rotate(-Math.PI / 2, START_COL * cellWidth + 16, getHeight() / 2.0);
+        g2.drawString("START", START_COL * cellWidth, getHeight() / 2);
+        g2.dispose();
 
-        // Draw FINISH text vertically
+        // FINISH label
         g2 = (Graphics2D) g.create();
         g2.setColor(Color.WHITE);
         g2.setFont(new Font("Arial", Font.BOLD, 16));
-        g2.rotate(-Math.PI / 2, finishCol * cellWidth +16, getHeight() / 2);
-        g2.drawString("FINISH", finishCol * cellWidth + 5, getHeight() / 2);
+        g2.rotate(-Math.PI / 2, FINISH_COL * cellWidth + 16, getHeight() / 2.0);
+        g2.drawString("FINISH", FINISH_COL * cellWidth + 5, getHeight() / 2);
         g2.dispose();
-    
+        
+    } finally {
+        trackLock.unlock();
     }
-    
+    }
+
     @Override
     public Dimension getPreferredSize() {
-    	return new Dimension(900, 450);
+        return new Dimension(900, 450);
     }
-    
-    //return method for main method
-    public List<car> getCars(){
-    	return cars;
-    }  
-    
-    public int getFinishCol() {
-    	return finishCol;
-    }
-    
-}
 
+    public List<Car> getCars() {
+        return cars;
+    }
+}
