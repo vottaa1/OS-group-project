02c0a304b42b9cf4a97d7fe39084def901eecc65
annanguyen2671603 <anhnguyen2671603@gmail.com>
2025-10-31 04:21:15 -0400

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,18 @@
+package Game;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Car {
+    int row;
+    int col;
+    Color color;
+
+    public Car(int row, Color color) {
+        this.row = row;
+        this.col = 0;
+        this.color = color;
+    }
+    
+}
